8c951a71a4398b94e7e11930911b8b862687a36d
jkrrrr <christopherharland@protonmail.com>
2023-04-24 15:28:11 +0100

    Fixed tab insert when not autocompleting

--- lua/configs/cmp.lua
+++ lua/configs/cmp.lua
@@ -28,8 +28,8 @@ local cmp = require'cmp'
 		["<Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
-      elseif vim.fn["vsnip#available"](1) == 1 then
-        feedkey("<Plug>(vsnip-expand-or-jump)", "")
+      --elseif vim.fn["vsnip#available"](1) == 1 then
+      --  feedkey("<Plug>(vsnip-expand-or-jump)", "")
       elseif has_words_before() then
         cmp.complete()
       else
